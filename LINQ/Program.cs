56b586b66e3d2c72e3a9efe830fcb86f0359129d
Robert Schofield <rschofield1992@gmail.com>
2024-04-10 16:03:37 -0400

    commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
+
+
+       
+       
+
+
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {    
+          List<string> myCities = new List<string>() { "Daytona","Ocala","Orlando","Tampa" ,"Gainsville", };
+
+
+            //sampleList = sampleList.OrderBy(s => s.Length).ToList();
+
+            myCities = myCities.OrderBy(s => s.Length).ToList();
+
+           foreach(var item in myCities)
+            {
+                Console.WriteLine(item);
+            }
+           
         }
     }
 }
